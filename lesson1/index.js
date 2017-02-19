03b5fc8ba4ef3653c1ba64355516472565d5e90b
the_fear <pasha-pivo@yandex.ru>
2017-02-19 19:22:23 +0500

    Made this module buggy and non-working

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,4 +1,38 @@
-function lesson1() {
-    return 'Hello, World';
-}
-module.exports = lesson1;
+'use strict';
+const fs = require('fs');
+const chalk = require('chalk');
+
+const regexpHell = () => {
+    const promise = new Promise((resolve, reject) => {
+
+        fs.readFile('package.json', function (err, buff) {
+            if (err) {
+                reject(err);
+            }
+            /** @var {string} str */
+            var str = buff.toString(),
+                    res = str.replace(/^\s*"[\w\d-]+":/gmi, chalk.blue('$&'));
+            res = res.replace(/[{}]+/gm, chalk.yellow('$&'));
+            res = res.replace(/^\s*"[\w\d]+"/gmi, chalk.cyan('$&'));
+            res = res.replace(/[\]\[](?!\d)/gm, chalk.yellow('$&'));
+            /*
+             * Ниже безуспешные попытки справиться с регулярками. Дошло, что управляющие
+             * последовательности ASCII сильно смахивают на символ юникода с "хвостиком"
+             * но развить "успех" не успел.
+             */
+//    res = res.replace(/([\d\w]m\s)!([:\s]*)(".*")$/gmi, '$1' + chalk.yellow('$2'));
+//    res = res.replace(/(\[\d+m)(\s*".*?")(,?)$/gmi, '$1' + chalk.yellow('$2') + '$3');
+//    reg = /(".*")\n/gmi;
+//    console.log(reg.exec(res));
+            resolve(res);
+        });
+    });
+    return promise;
+};
+regexpHell().then(result => {
+//    console.log(result);
+    module.exports = {result: result};
+    console.log(module);
+}, () => {
+    module.exports = 'Error opening the file';
+});
