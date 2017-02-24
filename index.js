a57ea3746fa0ef345fb34f3f2b3ce55fe83a43dc
the_fear <pasha-pivo@yandex.ru>
2017-02-24 13:27:25 +0500

    A bit of refactoring

--- index.js
+++ index.js
@@ -18,65 +18,81 @@ General usage:
 --bank=VALUE    for BlackJack only. Startup capital. If not specified, VALUE=100.
 --packs=VALUE   for BlackJack only. Number of packs of cards. Default value 1.
 --view-log      log file to view
+--lesson=NUMBER is used instead of --game to load the lesson module you need 
 `);
 };
-minimist.length = function () {
-    let result = 0;
-    for (let key in this){
-        if (!this.hasOwnProperty(key) || key === 'length'){
-            continue;
-        }
-        result += 1;
-    }
-    return result;
-};
+
 /**
- * Returns true if the entire input value is a valid number or float.
- * @example 
- * isNumber('123') // true <br>
- * isNumber('123foo') // false <br>
- * isNumber('123.45') // true <br>
- * @param {*} num Any type
- * @returns {Boolean} Remember, the function returns true if input is both '123' or 123
+ * Adds method 'length' to the minimist object to be able to calculate its length
  */
-function isNumber(num){
-    return !isNaN(num) && isFinite(num);
-}
+Object.defineProperty(minimist, 'length', {
+    value: function () {
+        return Object.keys(this).length;
+    },
+    enumerable: false
+});
 
 const blackJackSettings = {
     packs: 1,
     bank: 100
-}
+};
 let logFile = '', game;
+
+/**
+ * Returns true if the entire input value is a valid number or float.
+ */
+function isNumber(num) {
+    return !isNaN(num) && isFinite(num);
+}
 
 if (minimist.help) {
     help();
     process.exit(0);
 }
-if (minimist['view-log'] && minimist['view-log'] !== true){
-    let buff = fs.readFileSync(minimist['view-log']);
-    console.log(buff.toString());
-    process.exit(0);
-} else if (minimist['view-log'] === true) {
-    console.log('Log file is not specified.');
-    process.exit(1);
-}
-if (minimist.log && minimist.log !== true) {
-    logFile = minimist.log;
-}
-if (minimist.packs && minimist.packs !== true){
-    if (isNumber(minimist.packs)){
-        blackJackSettings.packs = parseInt(minimist.packs);
+
+if (minimist['view-log']) {
+    let log = minimist['view-log'];
+    switch (log) {
+        case true:
+            console.log('Log file is not specified.');
+            process.exit(1);
+            break;
+        default :
+        try {
+            let buff = fs.readFileSync(log);
+            console.log(buff.toString());
+            process.exit(0);
+        } catch (e) {
+            console.log(`No such file or directory: ${log}`);
+            process.exit(1);
+        }
     }
 }
-if (minimist.bank && minimist.bank !== true){
-    if (isNumber(minimist.bank)){
-        blackJackSettings.bank = parseInt(minimist.bank);
+if (minimist.log) {
+    switch (log) {
+        case true:
+            console.log('Log file is not specified. Aborterd.\n')
+            process.exit(1);
+            break;
+
+        default:
+            logFile = minimist.log;
     }
+}
+if (minimist.packs) {
+    let packs = minimist.packs;
+    packs = isNumber(packs) ? parseInt(packs) : 1;
+    blackJackSettings.packs = packs;
+}
+if (minimist.bank && minimist.bank !== true) {
+    let bank = minimist.bank;
+    bank = isNumber(bank) ? parseInt(bank) : 100;
+    blackJackSettings.bank = bank;
 }
 
-if (minimist.game && minimist.game !== true) {
-    switch (minimist.game.toLowerCase()) {
+if (minimist.game) {
+    let gameName = (typeof minimist.game === 'string') ? minimist.game.toLowerCase() : game;
+    switch (gameName) {
         case 'coin':
             game = require('./lesson2');
             game(logFile);
@@ -84,20 +100,19 @@ if (minimist.game && minimist.game !== true) {
         case 'blackjack':
             game = require('./blackjack/');
             game(blackJackSettings);
-//            console.log('This game is not implemented yet\n');
-//            process.exit(1);
+            break;
+        case true:
+            console.log('Game to load is not specified.');
+            process.exit(1);
             break;
         default:
-            console.log('Unknown game. Did you mean Coin?\n');
+            console.log('Unknown game\n');
             process.exit(1);
             break;
     }
-} else if (minimist.game === true) {
-    console.log('Game to load is not specified.');
-    process.exit(1);
 }
 
-if (minimist.length() === 1){
+if (minimist.length() === 1) {
     help();
 } else if (!game) {
     throw Error('Something weird has happened...');
